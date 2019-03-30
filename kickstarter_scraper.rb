956b546e4dcbce8d24755a35ab42cf365628043f
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-30 18:53:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,11 +1,21 @@
 require 'nokogirl'
 require 'pry'
 
-binding.pry
-kickstarter.css("li.project.grid_4").first
-learn 
 def create_project_hash
 html = File.read('fixtures/kickstarter.html')
-
 kickstarter = Nokogiri::HTML(html)
+
+projects = {}
+
+kickstarter.css("li.project.grid_4").each do |project|
+  title = project.css("h2.bbcard_name strong a").text
+  projects[title.to_syn] = { :image_link => project.css("div.project-thumbnail a img").attribute("scr").value,
+  :description => project.css("p.bbcard_blurb").text,
+  :location => project.css("ul.project-meta span.location-name").text
+  :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i}
 end
+
+def create_project_hash
+  @projects
+end
+  
